640105cafe3f4803cbe05f060d144fb163a40c21
Chibris007 <eihejirika4900@gmail.com>
2020-03-27 16:48:19 +0100

    task submission

---
+++ script.js
@@ -0,0 +1,14 @@
+let name = 'Emmanuel Chigozie Ihejirika';
+console.log(name);
+let courses = ['Html', 'Css', 'JavaScript'];
+console.log(courses);
+
+
+for(let i=1; i<=200; i++){
+    
+    if(i%2==0){
+        console.log(i);
+    }else{
+        console.log(i + ' is an odd number');
+    }
+}
